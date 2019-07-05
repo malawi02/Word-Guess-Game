87c937b4d1f1d7521d20edfed14e84847e047e4e
Zedekia Malawi <jatelo68@gmail.com>
2019-07-05 00:09:37 -0400

    taking a break

---
+++ assets/javascript/Tugo.js
@@ -0,0 +1,11 @@
+//create an array of words
+const words = ['pillates', 'skills', 'moods',];
+//choose words randomly
+Let randNum = Math.floor(Math.random() * words.length);
+let choosenWord = word[randNum];
+Console.log(choosenWord);
+//create underscores based on lenght of  words
+//Get users guess
+//check if guess is right 
+//if right push to right array
+//if wrong push to wrong array
