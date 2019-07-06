bb246451ef2cd9f5f98d657b32d64f8db1cb83b9
Zedekia Malawi <jatelo68@gmail.com>
2019-07-06 12:45:39 -0400

    made some changes in the js file

--- assets/javascript/Tugo.js
+++ assets/javascript/Tugo.js
@@ -1,10 +1,18 @@
 //create an array of words
-const words = ['pillates', 'skills', 'moods',];
+const words = ['thinking', 'workings', 'creating', 'jumping',];
 //choose words randomly
-Let randNum = Math.floor(Math.random() * words.length);
+let randNum = Math.floor(Math.random() * words.length);
 let choosenWord = word[randNum];
+let underscore = [];
 Console.log(choosenWord);
 //create underscores based on lenght of  words
+let generateUnderscore = ()=> {
+   for(let i =0; i < choosenWord.lenght; 1++ ){
+       underscore.puch('_');
+   }
+   return underscore;
+}
+console.log(generateUnderscore());
 //Get users guess
 //check if guess is right 
 //if right push to right array
